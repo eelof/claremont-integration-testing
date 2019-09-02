6db545c94053ec41dc7b4cb367ac423b83b64cef
Elof Bigestans <elof.bigestans@konsult.atg.se>
2019-09-02 12:35:01 +0200

    Some more IT's and cleanup

--- it/src/test/java/se/claremont/tutorial/weather/it/WeatherIT.java
+++ it/src/test/java/se/claremont/tutorial/weather/it/WeatherIT.java
@@ -42,8 +42,8 @@ public class WeatherIT {
         get("/weather/SE/Stockholm")
             .prettyPeek()
             .then()
-                .assertThat().content(containsString("soligt"))
-                .assertThat().content(containsString("Härligt!"));
+            .assertThat().content(containsString("soligt"))
+            .and().content(containsString("Härligt!"));
     }
 
     @Test
@@ -66,21 +66,26 @@ public class WeatherIT {
 
     @Test
     public void getWeather_invalidCountry_shouldReturnStatus400() {
-        mockServerClient.when(
-            request()
-                    .withMethod("GET")
-                    .withPath("/data/2.5/weather"))
-            .respond(
-                    response()
-                            .withHeader(new Header("Content-Type", "application/json; charset=utf-8"))
-                            .withBody(getResourceContents("openweather-responses/weather_stockholm_clear_19.json")
-                            .replaceAll("SE", "NonexistantCountry"))
-            );
-
-        get("/weather/SE/Stockholm")
+        get("/weather/XX/Stockholm")
                 .prettyPeek()
                 .then()
-                .assertThat().statusCode(200);
+                .assertThat().statusCode(400);
+    }
+
+    @Test
+    public void getWeather_cityNotFound_shouldReturnStatus404() {
+        get("/weather/SE/SomeCityThatDoesn'tExist")
+                .prettyPeek()
+                .then()
+                .assertThat().statusCode(404);
+    }
+
+    @Test
+    public void getWeather_invalidUrl_shouldReturnStatus404() {
+        get("/weathar/SE/Stockholm")
+                .prettyPeek()
+                .then()
+                .assertThat().statusCode(404);
     }
 
     private String getResourceContents(String path) {
